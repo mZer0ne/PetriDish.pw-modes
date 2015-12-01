fe5c9101a97febdf7730ca5a799fc70377baa980
mZer0ne <enternetacum@yandex.ru>
2015-12-01 02:00:14 +0200

    Update to version 1.3
    Update to version 1.3

--- launcher.user.js
+++ launcher.user.js
@@ -1,22 +1,19 @@
 // ==UserScript==
 // @name         PetriMods
 // @namespace    PetriMods Engine
-// @version      1.2
+// @version      1.3
 // @description  Улучшенный игровой процесс
 // @author       mZe
 // @match        http://petridish.pw/*
 // @grant        none
-// @run-at       document-body
 // ==/UserScript==
-var loadEngine = setInterval(function(){
-  if(window.jQuery){
-    clearInterval(loadEngine);
-    window.jQuery.ajax({
-        url: "https://api.github.com/repos/mZer0ne/PetriDish.pw-mods/git/refs/heads/master",
-        cache: false,
-        dataType: "jsonp"
-    }).done(function(data) {
-        window.jQuery.getScript('https://cdn.rawgit.com/mZer0ne/PetriDish.pw-mods/' + data.data.object.sha + '/mZeEngine.js');
-    }).fail(function() {alert('Error update PetriMod');window.location = 'http://vk.com/petrimods';});
-  }
-}, 10);
+$.ajax({
+  url: "https://api.github.com/repos/mZer0ne/PetriDish.pw-mods/git/refs/heads/master",
+  cache: false,
+  dataType: "jsonp"
+}).done(function(data) {
+  $.getScript('https://cdn.rawgit.com/mZer0ne/PetriDish.pw-mods/' + data.data.object.sha + '/mZeEngine.js');
+}).fail(function() {
+  alert('Error update PetriMod');
+  window.location = 'http://vk.com/petrimods';
+});
